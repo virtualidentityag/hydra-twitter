3ceae3b2bb42d3463c17d7e182305ed105270ccd
Matthias Steinböck <grillen@abendstille.at>
2013-08-08 17:53:01 +0200

    changed header information - different email in license information

--- src/VirtualIdentity/TwitterBundle/DependencyInjection/VirtualIdentityTwitterExtension.php
+++ src/VirtualIdentity/TwitterBundle/DependencyInjection/VirtualIdentityTwitterExtension.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Virtual-Identity Twitter package.
  *
- * (c) Virtual-Identity <development@virtual-identity.com>
+ * (c) Virtual-Identity <dev.saga@virtual-identity.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- src/VirtualIdentity/TwitterBundle/VirtualIdentityTwitterBundle.php
+++ src/VirtualIdentity/TwitterBundle/VirtualIdentityTwitterBundle.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the Virtual-Identity Twitter package.
+ *
+ * (c) Virtual-Identity <dev.saga@virtual-identity.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace VirtualIdentity\TwitterBundle;
 
